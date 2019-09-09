e5be07db23d776f5079520da0e6e0c8d6c51808f
ZhouZibo <tingsven@163.com>
2019-09-09 17:49:29 +0800

    compile first

--- bin/sourcemapping.js
+++ bin/sourcemapping.js
@@ -4,9 +4,47 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
     return cooked;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
+var path = require("path");
 var ErrorStackParser = require("error-stack-parser");
 var commander = require("commander");
+var fs_1 = require("fs");
+var source_map_1 = require("source-map");
 var raw_stack_string_array;
 function stackStringProcess(value, previous) {
     // input '\n' will be translated into '\\n' and cause ErrorStackParser parsing failure
@@ -15,41 +53,105 @@ function stackStringProcess(value, previous) {
 }
 function printToConsole(error_msg, stack_frames) {
 }
+function loadAllConsumer(dir_path, stack_frame_array, sourcemap_map) {
+    return __awaiter(this, void 0, void 0, function () {
+        var sourcemap_list, regExp, _i, stack_frame_array_1, frame, name_1, sourcemap_filepath, sourcemap, consumer;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    sourcemap_list = new Set();
+                    regExp = /.+\/(.+)$/;
+                    _i = 0, stack_frame_array_1 = stack_frame_array;
+                    _a.label = 1;
+                case 1:
+                    if (!(_i < stack_frame_array_1.length)) return [3 /*break*/, 4];
+                    frame = stack_frame_array_1[_i];
+                    if (!frame.hasOwnProperty('fileName')) return [3 /*break*/, 3];
+                    name_1 = regExp.exec(frame.fileName)[1];
+                    frame.fileName = name_1;
+                    if (!!sourcemap_list.has(name_1)) return [3 /*break*/, 3];
+                    sourcemap_list.add(name_1);
+                    sourcemap_filepath = path.join(dir_path, name_1 + '.map');
+                    sourcemap = void 0;
+                    try {
+                        sourcemap = JSON.parse(fs_1.readFileSync(sourcemap_filepath, 'utf-8'));
+                    }
+                    catch (error) {
+                        console.error('Read&Parse sourcemap:' + sourcemap_filepath + 'failed. ' + error.toString());
+                        process.exit(0);
+                    }
+                    return [4 /*yield*/, new source_map_1.SourceMapConsumer(sourcemap)];
+                case 2:
+                    consumer = _a.sent();
+                    !sourcemap_map.has(name_1) && sourcemap_map.set(name_1, consumer);
+                    _a.label = 3;
+                case 3:
+                    _i++;
+                    return [3 /*break*/, 1];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
 var program = new commander.Command();
 program.version('0.0.1', '-v, --version');
 program.option('-s, --stack <string>', 'stack string which can obtain from JSON.stringfy(Error.stack)', stackStringProcess);
 program.option('-i, --msg <string>', 'error message. e.g. Uncaught ReferenceError: a is not defined');
-program.option('-m, --map <string>', 'sourcemap dir. Where to find sourcemap');
+program.option('-m, --map <string>', 'sourcemap dir path. Where to find sourcemap');
 program.parse(process.argv);
 console.debug("Check input");
 console.log(program.opts());
-if (program.stack && program.msg) {
+if (program.stack && program.msg && program.map) {
     var error_obj = {
         'stack': program.stack,
         'message': program.msg,
         'name': program.msg.split(':')[0]
     };
     console.log(error_obj);
-    var stack_frame_array = [];
+    var stack_frame_array_2 = [];
     try {
-        stack_frame_array = ErrorStackParser.parse(error_obj);
+        stack_frame_array_2 = ErrorStackParser.parse(error_obj);
     }
     catch (error) {
         console.error('ErrorStackParser parsing failed' + error.toString());
         process.exit(0);
     }
-    console.log(stack_frame_array);
-    // TODO: Translate line/column/fileName using sourcemap
-    // stack_frame_array.forEach(stack_frame => {
-    // });
-    // let frames_count = stack_frame_array.length;
-    // let frames_done = 0;
-    // while(frames_done < frames_count){
-    //     const source_map = JSON.parse(readFileSync('','utf-8'));
-    // }    
+    console.log(stack_frame_array_2);
+    var sourcemap_map_1 = new Map();
+    // 加载全部要用到的sourcemap文件
+    loadAllConsumer(program.map, stack_frame_array_2, sourcemap_map_1).then(function () {
+        // let promise_list: Promise<any>[];
+        // 遍历解析stack_frame_array
+        stack_frame_array_2.forEach(function (stack_frame) {
+            var name = stack_frame.fileName;
+            var consumer = sourcemap_map_1.get(name);
+            var origin = consumer.originalPositionFor({
+                line: stack_frame.lineNumber,
+                column: stack_frame.columnNumber
+            });
+            if (origin.line)
+                stack_frame.lineNumber = origin.line;
+            if (origin.column)
+                stack_frame.columnNumber = origin.column;
+            if (origin.source)
+                stack_frame.fileName = origin.source;
+            if (origin.name)
+                stack_frame.functionName = origin.name;
+        });
+        // Promise.all(promise_list).then(() => {
+        //     
+        // });
+        // 打印结果
+        stack_frame_array_2.toString();
+    });
+    // 解析结束后destroy所有consumer
+    for (var _i = 0, _a = Array.from(sourcemap_map_1.values()); _i < _a.length; _i++) {
+        var consumer = _a[_i];
+        consumer.destroy();
+    }
 }
 else {
-    console.error("No error stack string or error msg string found. Please Check input.");
+    console.error("No error stack string OR error msg string OR sourcemap dir found. Please Check input.");
 }
 var templateObject_1;
 // stack: "ReferenceError: exclued is not defined\n    at getParameterByName (http://localhost:7777/aabbcc/index.js:7:37)\n    at http://localhost:7777/aabbcc/index.js:15:11",
@@ -64,3 +166,4 @@ var templateObject_1;
 //     lineNumber: 15,
 //     fileName: 'http://localhost:7777/aabbcc/index.js',
 //     source: '    at http://localhost:7777/aabbcc/index.js:15:11' } ]
+//# sourceMappingURL=sourcemapping.js.map
